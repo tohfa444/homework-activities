adbb2edcc0ec79fca324486ec826d9ea4bd3bfd6
tohfa444 <svoil.flour@gmail.com>
2025-04-17 23:19:58 +0600

    initial commit

---
+++ L10.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+c=int(input("Enter the third number"))
+
+print(f"befor swapping \na was {a} \nb was {b} \nc was {c}")
+a,b,c=b,c,a
+print(f"After swapping \na is {a} \nb is {b} \nc is {c}")
