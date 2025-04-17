f2bea7c67698efc456c00ef6404e83bd97088e7d
tohfa444 <svoil.flour@gmail.com>
2025-04-17 23:33:52 +0600

    initial commit

--- L2.py
+++ L2.py
@@ -1,13 +1,6 @@
-print("hello I'm tohfa and I want know your birthday dates")
-print("roy")
-roy=input("enter your birthday date ")
-print("simthiya")
-simthiya=input("enter your birthday date ")
-print("anisa")
-anisa=input("enter your birthday date ")
-print("sharuk")
-sharuk=input("enter your birthday date ")
-print("nithi")
-nithi=input("enter your birthday date ")
-print("i collected my friend's birthday dates \n roy's birthday date is ",roy, "\n simthiya's birthday date is ",simthiya," \n anisa's birthday date is ",anisa,"\n sharuk's birthday date is ",sharuk," \n nithi's birthday date is ",nithi)
-
+number=int(input("Give me a number then I'll check it's odd or even"))
+result=number%2
+if result==0:
+    print("It's an even number")
+else:
+    print("It's an odd number")
