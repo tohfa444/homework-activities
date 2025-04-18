a8f0919b4709698d2fd44f47cf4263b9cd6034fd
tohfa444 <svoil.flour@gmail.com>
2025-04-18 15:30:36 +0600

    initial commit

---
+++ L7.py
@@ -0,0 +1,8 @@
+tem=int(input("Enter the tepareture in celsius"))
+
+if tem<0:
+    print("You should wera hot clothes and boots and muflers etc")
+elif tem<10:
+    print("You don't have to wear heavy jackets but wear a few hot clothes")
+else:
+    print("You sould wear light and soft clothes in this weather")
