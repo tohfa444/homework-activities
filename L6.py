f9648f1cd211a7a4e65f95087528409db00a1d56
tohfa444 <svoil.flour@gmail.com>
2025-04-18 15:36:39 +0600

    initial commit

---
+++ L6.py
@@ -0,0 +1,9 @@
+import math
+
+number=int(input("Enter a number: "))
+
+if number >= 0:
+    sqrt=math.sqrt(number)
+    print(f"The square root of {number} is {sqrt}")
+else:
+    print("Sorry, square root of a negative number is not real.")
