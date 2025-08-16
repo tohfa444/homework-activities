fc0911bade503ada388cc5403ef9ec6bb29b16a4
tohfa444 <svoil.flour@gmail.com>
2025-08-16 15:35:25 +0600

    initial commit

---
+++ L20.py
@@ -0,0 +1,22 @@
+class BMW:
+    def start(self):
+        print("BMW engine started.")
+
+    def speed(self):
+        print("BMW top speed is 250 km/h.")
+
+
+class Ferrari:
+    def start(self):
+        print("Ferrari engine started.")
+
+    def speed(self):
+        print("Ferrari top speed is 350 km/h.")
+
+bmw_car = BMW()
+ferrari_car = Ferrari()
+
+for car in (bmw_car, ferrari_car):
+    car.start()
+    car.speed()
+    print()  
