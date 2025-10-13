e1712b7fd3a9898dd32aa270374668372e933988
tohfa444 <svoil.flour@gmail.com>
2025-10-13 17:03:02 +0600

    initial commit

---
+++ L29.py
@@ -0,0 +1,15 @@
+# Program to store birthday dates in 5 different variables
+
+birthday_1 = "January 5, 1995"
+birthday_2 = "March 12, 2000"
+birthday_3 = "June 23, 1998"
+birthday_4 = "September 10, 2003"
+birthday_5 = "December 25, 1990"
+
+# Display the stored birthdays
+print("Birthday 1:", birthday_1)
+print("Birthday 2:", birthday_2)
+print("Birthday 3:", birthday_3)
+print("Birthday 4:", birthday_4)
+print("Birthday 5:", birthday_5)
+
