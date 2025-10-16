445897201b79fee382be4ab0dc001e5df6bab248
tohfa444 <svoil.flour@gmail.com>
2025-10-16 18:13:50 +0600

    initial commit

---
+++ L39.py
@@ -0,0 +1,8 @@
+# Get age input from the user
+age = int(input("Enter your age: "))
+
+# Check if age is between 10 and 20
+if 10 <= age <= 20:
+    print("Your age is between 10 to 20.")
+else:
+    print("Your age is not between 10 to 20.")
